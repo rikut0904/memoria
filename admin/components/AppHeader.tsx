6b72cd5f705aaae3dbe712774faac75ab236a029
rikut0904 <rkt9tuba4robot@gmail.com>
2026-02-10 18:12:21 +0900

    fix/error

--- admin/components/AppHeader.tsx
+++ admin/components/AppHeader.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { ReactNode } from 'react'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import api from '@/lib/api'
 import { clearAuthToken, clearRefreshToken } from '@/lib/auth'
